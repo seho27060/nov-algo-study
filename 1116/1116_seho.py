249e973526af842a0d070322a7959e20b04e5eb4
seho <seho27060@gmail.com>
2022-11-17 22:30:28 +0900

    1116 seho

--- 1116/1116_seho.py
+++ 1116/1116_seho.py
@@ -1,27 +1,32 @@
 # 221116
-# 맹~?
-
-m ="ABCDEFG"
-musicinfos = ["12:00,12:14,HELLO,CDEFGAB", "13:00,13:05,WORLD,ABCDEF","12:50,13:01,A,BBBB"]
-
-m = "CC#BCC#BCC#BCC#B"
-musicinfos = ["03:00,03:30,FOO,CC#B", "04:00,04:08,BAR,CC#BCC#BCC#B"]
+# 파싱 + 구현 문제
 
 def solution(m, musicinfos):
     answer = "(None)"
     musicTime = 0
 
-    origin = ["C","D","E","F","G","A","B"]
     other = ["C#","D#","F#","G#","A#"]
+
+    playedMusic = []
+    for idx in range(len(m)):
+        check = False
+        if m[idx] != "#":
+            if idx < len(m) - 1:
+                if m[idx:idx + 2] in other:
+                    playedMusic.append(m[idx:idx+2])
+                    check =True
+            if check == False:
+                playedMusic.append(m[idx])
+
     for musicinfo in musicinfos:
         start,end,name,sheet = musicinfo.split(",")
         hour = int(end[:2]) - int(start[:2])
         minutes = hour*60 + (int(end[3:5]) - int(start[3:5]))
-        print(musicinfo,minutes)
         idx = 0
         music = []
+        tictok = minutes
         # 시간만큼의 재생음악 생성
-        while minutes > 0:
+        while tictok > 0:
             check = False
             if idx >= len(sheet):
                 idx = 0
@@ -33,10 +38,24 @@ def solution(m, musicinfos):
             if check == False:
                 music.append(sheet[idx])
                 idx += 1
-            minutes -= 1
+            tictok -= 1
 
-        print(len(music),music)
+        for soundIdx in range(len(music)):
+            if music[soundIdx] == playedMusic[0] and soundIdx + len(playedMusic) <= len(music):
+                if "".join(playedMusic) == "".join(music[soundIdx:soundIdx+len(playedMusic)]):
+                    if minutes > musicTime:
+                        answer = name
+                        musicTime = minutes
+                        break
 
     return answer
 
-print(solution(m, musicinfos))
+
+
+
+
+
+
+
+
+
