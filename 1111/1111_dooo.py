20c869610c5c27a0b8210a08d5a21560579f1a56
seongdo Byun <dmdsns@naver.com>
2022-11-30 15:53:42 +0900

    1111 dooo

---
+++ 1111/1111_dooo.py
@@ -0,0 +1,23 @@
+def solution(info, edges):
+    visited = [0] * len(info)
+    answer = []
+    visited[0] = 1
+
+    def dfs(sheep, wolf):
+        if sheep > wolf:
+            answer.append(sheep)
+        else:
+            return
+
+        for p, c in edges:
+            if visited[p] and not visited[c]:
+                visited[c] = 1
+                if info[c] == 0:
+                    dfs(sheep + 1, wolf)
+                else:
+                    dfs(sheep, wolf + 1)
+                visited[c] = 0
+
+    dfs(1, 0)
+
+    return max(answer)
